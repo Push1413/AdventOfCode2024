3bd4b57e2bf1b8a6196babfd9a92d2543250573c
Pushkar Aagnihotri <agnihotripush@gmail.com>
2024-12-02 14:28:56 -0800

    :zap: day02

---
+++ src/org/devpush/com/day02/Helper.java
@@ -0,0 +1,37 @@
+package org.devpush.com.day02;
+
+import java.util.List;
+
+public class Helper {
+
+    public boolean isRowSafe(List<Integer> reports){
+        boolean isIncreasing = false;
+        isIncreasing = reports.get(0) <= reports.get(1);
+
+        if (isIncreasing){
+            for (int i = 1; i < reports.size(); i++) {
+                int diff = reports.get(i)- reports.get(i-1);
+                if (diff>0 && diff <4){
+                    continue;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        else {
+            for (int i = 1; i < reports.size(); i++) {
+                int diff = reports.get(i-1)- reports.get(i);
+                if (diff>0 && diff <4){
+                    continue;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+}
---
+++ src/org/devpush/com/day02/day2.java
@@ -0,0 +1,60 @@
+package org.devpush.com.day02;
+
+import org.devpush.com.Utils;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class day2 {
+    static int safeReports =0;
+    public static void main(String[] args) throws IOException {
+        List<List<Integer>> ipForSecond = firstStar();
+        secondStar(ipForSecond);
+    }
+
+    private static List<List<Integer>> firstStar() throws IOException {
+        List<String> lines = Utils.readInput(false,2,false);
+        Pattern pattern = Pattern.compile("(\\d+)");
+        List<List<Integer>> unSafeReportsList = new ArrayList<>();
+        for (String line:lines){
+            if (line.isEmpty())continue;
+            List<Integer> reports = new ArrayList<>();
+            Matcher m = pattern.matcher(line);
+            while (m.find()){
+                reports.add(Integer.parseInt(m.group()));
+            }
+//            System.out.println(Arrays.toString(reports.toArray()));
+            Helper helper = new Helper();
+            if (helper.isRowSafe(reports)){
+                safeReports++;
+            }
+            else {
+                unSafeReportsList.add(reports);
+            }
+        }
+        System.out.println(safeReports);
+        return unSafeReportsList;
+
+    }
+
+    private static void secondStar(List<List<Integer>> unSafeReportList)  {
+        for (List<Integer> report:unSafeReportList){
+            for (int i = 0; i < report.size(); i++) {
+                List<Integer> modifiedList = new ArrayList<>(report);
+                modifiedList.remove(i);
+                Helper helper = new Helper();
+                if (helper.isRowSafe(modifiedList)){
+                    safeReports++;
+                    break;
+                }
+            }
+        }
+        System.out.println(safeReports);
+
+    }
+
+
+}
